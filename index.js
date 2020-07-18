a4caeda9797982d3e9310fc1022eb61f532779aa
jonnred <67404290+jonnred@users.noreply.github.com>
2020-07-18 19:38:40 +0800

    image slider practice

---
+++ index.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    const prevbtn = document.querySelector("#prevbutton");
+    prevbtn.addEventListener("click" , function(){
+        document.querySelector("#a").style.opacity="1";
+    });
+
+}
